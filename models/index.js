486806c7714c77c0ed56c52d8434ccfae5aae866
Adam Back <adb33@email.arizona.edu>
2016-09-17 07:54:44 -0700

    (feat) Added plug model.

---
+++ models/index.js
@@ -0,0 +1,42 @@
+var fs        = require( 'fs');
+var path      = require( 'path' );
+var Sequelize = require( 'sequelize' );
+var basename  = path.basename( module.filename );
+var db        = {};
+var config    = require( '../config' );
+var sequelize = new Sequelize( config.database, config.username, config.password, config );
+
+//////////////////
+// Import Schemas
+//////////////////
+
+// get all the files in the models/ directory
+fs.readdirSync( __dirname )
+  .filter( function( file ) {
+    // filter out hidden files & this file
+    return ( file.indexOf( '.' ) !== 0 ) && ( file !== basename );
+  })
+  // for each of the files
+  .forEach( function( file ) {
+    // import it
+    var model = sequelize.import( path.join( __dirname, file ) );
+    // set it to the db object
+    db[ model.name ] = model;
+  });
+
+Object.keys( db ).forEach( function( modelName ) {
+  // associate as needed
+  if ( "associate" in db[ modelName ] ) {
+    db[ modelName ].associate( db );
+  }
+});
+
+db.sequelize.sync()
+.then(function() {
+  console.log( 'Success sync on startup' );
+})
+.catch(function( error ) {
+  console.log( 'ERROR: sync on startup, ', error );
+});
+
+module.exports = db;
