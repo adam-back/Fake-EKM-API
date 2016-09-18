e9b348301738aca3aa5b4b4b366adb651a429861
Adam Back <adb33@email.arizona.edu>
2016-09-17 21:05:00 -0400

    (fix) Required fakeData generator wrong.

--- controllers/push.controller.js
+++ controllers/push.controller.js
@@ -1,11 +1,11 @@
-var fakeData = require( '../helpers/fakeDataGenerator' );
+var generateFakeData = require( '../helpers/fakeDataGenerator' );
 var models   = require( '../models' );
 
 module.exports = exports = {
   getRealTimeData: function ( req, res, next ) {
     models.plug.findAll( { where: { ekm_omnimeter_serial: { $ne: null } }, raw: true } )
     .then(function( plugs ) {
-      var response = fakeData.generateFakeResponse( plugs, req.params.version, req.params.key );
+      var response = generateFakeData( plugs, req.params.version, req.params.key );
       res.json( response );
     })
     .catch(function( error ) {
